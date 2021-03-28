92059dbc384df99471ca3102f4b6ea7024333ba7
Aaron <talentyupush@163.com>
2021-03-28 23:10:40 +0800

    modif logo

--- __ENV.js
+++ __ENV.js
@@ -1,1 +1,1 @@
-window.__ENV = {"REACT_APP_KAI_API":"https://api.nsure.network/v1","REACT_APP_ZHAO_API":"http://192.168.0.150:8001"};
+window.__ENV = {"REACT_APP_KAI_API":"https://api.nsure.network/v1","REACT_APP_ZHAO_API":"https://api.nsure.network/claim","REACT_APP_CLAIM":"https://claim.nsure.network"};
